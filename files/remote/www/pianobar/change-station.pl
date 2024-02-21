3f4b90334a69ca4d57405b678b9e887bca49bc0e
Aaron Bugher <aabugher@gmail.com>
2024-02-20 21:18:45 -0500

    sed's "in place" (-i) operation actually replaces the file with a new one.  The new one does not (and cannot) retain group membership.  That caused a serious bug where instead of changing stations, the change station button would make the config file unreadable to pianobar and stop the music.  Replace use of sed with ... Perl.

--- files/remote/www/pianobar/change-station.pl
+++ files/remote/www/pianobar/change-station.pl
@@ -20,7 +20,18 @@ if( 0 != $ret ) {
   die "Error:  Failed to stop pianobar: ${ret}";
 }
 my $real_config = readlink( '/etc/pianobard/config' );
-system( "sed -Ei 's/autostart_station = [0-9]+/autostart_station = ${station}/' '/etc/pianobard/${real_config}'" );
+my $real_config_path="/etc/pianobard/${real_config}";
+open(my $real_config_in, '<', $real_config_path) or die "Error:  Could not open config file (r):  ${real_config_path}";
+my @old_config_lines = do { local $/; <$real_config_in> };
+my @new_config_lines;
+for(@old_config_lines) {
+  my $new_line = $_;
+  $new_line =~ s/autostart_station = [0-9]+/autostart_station = ${station}/;
+  push(@new_config_lines, $new_line );
+}
+open(my $real_config_out, '>', $real_config_path) or die "Error:  Could not open config file (w):  ${real_config_path}";
+print{$real_config_out} @new_config_lines;
+
 system( 'sudo /etc/init.d/pianobar start >/dev/null 2>&1' );
 $ret = ( $? >> 8 );
 if( 0 != $ret ) {
